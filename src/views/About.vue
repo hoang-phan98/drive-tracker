b145bfb41049c03a610006092bb34cd47ebc8fff
Erica Son <eson0001@student.monash.edu>
2018-08-29 01:04:16 +1000

    Base UI finished
    Touch ups still needed

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="folderList">
-    <b-container>
+    <b-container> 
       <b-row>
         <b-col>
 
         </b-col>
-        <b-col cols="8">
+        <b-col cols="6">
           <h1>Current Folders</h1>
-          <ul>
-            <b-row v-for="folder in folderList" :key="folder.id">{{ folder.name}}</b-row>
-          </ul>
-          <b-button>Add a Folder</b-button>
-          
 
-          <b-form-group label="Enter Google Drive Folder URL">
-            <b-form-input placeholder="Enter Folder URL"></b-form-input>
-          </b-form-group>
+          <span> <br> </span>
+
+          <b-row class="rows" v-for="folder in folderList" :key="folder.id">
+            <b-button class="folderButton">{{ folder.name }}</b-button>
+          </b-row>
+          
+          <span> <br> </span>
+
+          <b-row class="rows">
+            <b-button class="button">Add a Folder</b-button>
+          </b-row>
         </b-col>
         <b-col>
 
@@ -34,9 +37,10 @@ export default {
   data() {
     return {
       folderList: [
-        { id: 0, name: "file1" },
-        { id: 1, name: "file2" },
-        { id: 2, name: "file3" }
+        { id: 0, name: "FIT2101" },
+        { id: 1, name: "FIT1049" },
+        { id: 2, name: "Assignment 1" },
+        { id: 3, name: "Hello" }
       ]
     };
   }
@@ -44,3 +48,29 @@ export default {
 </script>
 
 <!--v-bind:folder="folder" v-bind:key="folder.id-->
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.rows {
+  list-style-type: none;
+  margin: 2%;
+}
+.folderButton {
+  background-color: whitesmoke;
+  border-color: whitesmoke;
+  color: darkslategray;
+  width: 100%;
+  font-size: 18pt;
+}
+.button {
+  background-color: rgb(190, 231, 163);
+  color: darkslategrey;
+  width: 100%;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container> 
+      <b-row>
+        <b-col>
+
+        </b-col>
+        <b-col cols="8">
+          <h1>Add a Folder</h1>
+
+        </b-col>
+        <b-col>
+
+        </b-col>
+      </b-row>
+      <b-row>
+        <span> <br> </span>
+      </b-row>
+      <b-row>
+        <b-col>
+
+        </b-col>
+        <b-col cols="8">
+
+          <b-form-group label="Enter Google Drive Folder URL">
+            <b-form-input placeholder="Enter Folder URL (back button in menu bar)"></b-form-input>
+          </b-form-group>
+          <b-button>Submit</b-button>
+        </b-col>
+        <b-col>
+
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
