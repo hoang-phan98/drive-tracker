cd195dd830309ee367cc4c5768adc7fc19d106e9
Hoang Phan <hpha0005@student.monash.edu>
2018-08-29 19:55:26 +1000

    Removing Vue default template from the page

--- src/App.vue
+++ src/App.vue
@@ -33,13 +33,6 @@
 
           </b-collapse>
         </b-navbar>
-      <div id="app">
-        <div id="nav">
-          <router-link to="/">Home</router-link> |
-          <router-link to="/about">About</router-link>
-        </div>
-        <router-view/>
-      </div>
     </div>
 </template>
 
