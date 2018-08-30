11b0f4732e93d29a68399a674e75480dc92bdd61
Kenny Ye <kyee0003@student.monash.edu>
2018-08-31 00:16:46 +1000

    UI designs started
    initial works for the UI in the login feature without designs.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="login">
-    <g-signin-button
-      :params="googleSignInParams"
-      @success="onSignInSuccess"
-      @error="onSignInError">
-      Sign in with Google
-    </g-signin-button>
+    <div class="login-box">
+      <img src="../assets/monash.png">
+      </br></br></br>
+      <font size="6">"As we look ahead into the next century, leaders will be those who empower others."</br>--Bill Gates</font>
+      </br></br></br>
+      <g-signin-button
+        :params="googleSignInParams"
+        @success="onSignInSuccess"
+        @error="onSignInError">
+        <img id="google-logo" src="../assets/google-logo.png">
+        <strong>Sign in with Google</strong>
+      </g-signin-button>
+    </div>
     <div class=tint></div>
     <div class=background></div>
   </div>
@@ -45,10 +52,33 @@ export default {
 </script>
 
 <style>
+.login img {
+  width: 50%;
+  height: auto;
+}
+
+#google-logo {
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+  margin-left: 2px;
+}
+
+.login-box {
+  background: rgba(256, 256, 256, 1); /*can be anything, of course*/
+  margin-right: 550px;
+  margin-left: 550px;
+  padding: 50px;
+  box-shadow: 0px 0px 46px -5px rgba(0,0,0,0.75);
+  border-radius: 25px;
+
+}
+
 .g-signin-button {
   /* This is where you control how the button looks. Be creative! */
   display: inline-block;
-  padding: 4px 8px;
+  padding: 5px 16px;
+  padding-left: 4px;
   border-radius: 3px;
   background-color: #3c82f7;
   color: #fff;
