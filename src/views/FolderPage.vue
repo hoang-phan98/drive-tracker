7e0e670d7a347b7b1ade600f5730bf69d144369a
Kenny Ye <kyee0003@student.monash.edu>
2018-09-01 00:49:19 +1000

    Tab bar controller added
    The tab bar controller for the file contribution functionality has been put in. No information is shown, just the tab bar controller for now.

--- src/views/FolderPage.vue
+++ src/views/FolderPage.vue
@@ -11,21 +11,42 @@
         histogram goes here
       </div>
       <div class="filecontribution">
-        Contribution for each file in the folder goes here
+        <b-card no-body>
+          <b-tabs card>
+            <b-tab title="Day" active>
+              File Contribution bar graph for day goes here
+            </b-tab>
+            <b-tab title="Week">
+              File Contribution bar graph for week goes here
+            </b-tab>
+            <b-tab title="Month">
+              File Contribution bar graph for month goes here
+            </b-tab>
+            <b-tab title="Year">
+              File Contribution bar graph for year goes here
+            </b-tab>
+            <b-tab title="All Time">
+              File Contribution bar graph for alltime goes here
+            </b-tab>
+          </b-tabs>
+        </b-card>
       </div>
     </div>
   </div>
 </template>
 
+<script>
+</script>
+
 <style>
 .grid-container {
   display: grid;
-  grid-gap: 30px 30px;
+  grid-gap: 15px 15px;
 }
 
 .users {
   background: rgba(256, 256, 256, 1); /*can be anything, of course*/
-  width: 40%;
+  width: 90%;
   margin-left: auto;
   margin-right: auto;
   grid-column: 1 / 2;
@@ -37,7 +58,7 @@
 }
 .pichart {
   background: rgba(256, 256, 256, 1); /*can be anything, of course*/
-  width: 50%;
+  width: 90%;
   margin-left: auto;
   margin-right: auto;
   grid-column: 2 / 3;
@@ -49,7 +70,7 @@
 }
 .histogram {
   background: rgba(256, 256, 256, 1); /*can be anything, of course*/
-  width: 80%;
+  width: 90%;
   margin-left: auto;
   margin-right: auto;
   grid-column: 1 / 3;
@@ -61,7 +82,7 @@
 }
 .filecontribution {
   background: rgba(256, 256, 256, 1); /*can be anything, of course*/
-  width: 80%;
+  width: 90%;
   margin-left: auto;
   margin-right: auto;
   grid-column: 1 / 3;
