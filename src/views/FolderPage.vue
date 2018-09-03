ff6d71310b70d7df9511d7d6bff9ee1c40c49790
Kenny Ye <kyee0003@student.monash.edu>
2018-09-03 23:28:47 +1000

    Stacked bar chart created
    A stack bar chart with mock data created and put inside the folder page UI in appropriate location

--- src/views/FolderPage.vue
+++ src/views/FolderPage.vue
@@ -2,7 +2,11 @@
   <div class="folderpage">
     <div class="grid-container">
       <div class="users">
-        users go here
+        <GChart
+          type="BarChart"
+          :data="chartData"
+          :options="chartOptions"
+        />
       </div>
       <div class="pichart">
         pi chart goes here
@@ -36,6 +40,34 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import VueGoogleCharts from 'vue-google-charts';
+
+Vue.use(VueGoogleCharts)
+
+export default {
+  data () {
+    return {
+      // Array will be automatically processed with visualization.arrayToDataTable function
+      chartData: [
+        ['Genre', 'Fantasy & Sci Fi', 'Romance', 'Mystery/Crime', 'General', 'Western', 'Literature', { role: 'annotation' } ],
+        ['Kenny', 10, 24, 20, 32, 18, 5, ''],
+        ['Hoang', 16, 22, 23, 30, 16, 9, ''],
+        ['Erica', 28, 19, 29, 30, 12, 13, ''],
+        ['Dax', 16, 22, 23, 30, 16, 9, ''],
+        ['Marc', 28, 19, 29, 30, 12, 13, '']
+      ],
+      chartOptions: {
+        width: 1000,
+        height: 400,
+        legend: { position: 'top', maxLines: 3 },
+        bar: { groupWidth: '75%' },
+        isStacked: true
+      }
+    }
+  }
+}
+
 </script>
 
 <style>
@@ -46,49 +78,38 @@
 
 .users {
   background: rgba(256, 256, 256, 1); /*can be anything, of course*/
-  width: 90%;
-  margin-left: auto;
-  margin-right: auto;
+  padding: 0px;
   grid-column: 1 / 2;
   grid-row: 1 / 2;
   text-align: center;
-  padding: 50px;
   box-shadow: 0px 0px 46px -5px rgba(0, 0, 0, 0.75);
   border-radius: 25px;
 }
+
 .pichart {
   background: rgba(256, 256, 256, 1); /*can be anything, of course*/
-  width: 90%;
-  margin-left: auto;
-  margin-right: auto;
+  padding: 0px;
   grid-column: 2 / 3;
   grid-row: 1 / 2;
   text-align: center;
-  padding: 50px;
   box-shadow: 0px 0px 46px -5px rgba(0, 0, 0, 0.75);
   border-radius: 25px;
 }
 .histogram {
   background: rgba(256, 256, 256, 1); /*can be anything, of course*/
-  width: 90%;
-  margin-left: auto;
-  margin-right: auto;
+  padding: 80px;
   grid-column: 1 / 3;
-  grid-row: 2 / 3;
+  grid-row: 3 / 4;
   text-align: center;
-  padding: 50px;
   box-shadow: 0px 0px 46px -5px rgba(0, 0, 0, 0.75);
   border-radius: 25px;
 }
 .filecontribution {
   background: rgba(256, 256, 256, 1); /*can be anything, of course*/
-  width: 90%;
-  margin-left: auto;
-  margin-right: auto;
+  padding: 80px;
   grid-column: 1 / 3;
-  grid-row: 3 / 4;
+  grid-row: 4 / 5;
   text-align: center;
-  padding: 50px;
   box-shadow: 0px 0px 46px -5px rgba(0, 0, 0, 0.75);
   border-radius: 25px;
 }
