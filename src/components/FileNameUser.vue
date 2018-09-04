c4ce10ee120165d7ce9942502f74e9d307d1a71b
Erica Son <eson0001@student.monash.edu>
2018-09-04 23:24:58 +1000

    Added more detailed comments
    Detailed comments added

--- src/components/FileNameUser.vue
+++ src/components/FileNameUser.vue
@@ -1,28 +1,28 @@
 <template>
     <div>
         <b-row v-for="file in fileList" :key="file.id">
+          <!-- entire file object -->
           <b-col> {{ file }} </b-col>
-            <!-- note here we choose the file names in the selected foler-->
-            <b-col> {{ file.name }} </b-col>
+            <!-- filename -->
+          <b-col> {{ file.name }} </b-col>
 
-            <b-col v-for="user in file.permissions" :key="user.id">
-                    {{user.displayName}}
-                    {{user.id}}
-                </b-col>
-            <span>-</span>
-            <span><br></span>
-            
-            <b-col>{{file.writers}}</b-col>
+          <b-col v-for="user in file.permissions" :key="user.id">
+              <!-- user name and id -->
+            {{user.displayName}}
+            {{user.id}}
+          </b-col>
+          <span><br></span>
         </b-row>
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import gapi from "../googleapis.js";
 
 export default {
   name: "folderList",
+  // fileList is an object with the file's id and permissions
+  // permissions has the user's id and display name that we can use for the displaying of data
   async mounted() {
     this.fileList = (await gapi.client.drive.files.list({
       fields: "files(id, name, permissions)",
