6b9ffaafdddad754f341414aa64a9b46a700bfc0
Erica Son <eson0001@student.monash.edu>
2018-09-04 23:21:46 +1000

    File names and users extracted from folder
    visit localhost:8080/data to find data (object, name, user and user id) extracted from a folder who's id is specified in line with appropriate comment

--- src/components/FileNameUser.vue
+++ src/components/FileNameUser.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
         <b-row v-for="file in fileList" :key="file.id">
+          <b-col> {{ file }} </b-col>
             <!-- note here we choose the file names in the selected foler-->
             <b-col> {{ file.name }} </b-col>
-            <!--<b-col>{{file}}</b-col>-->
-                <b-col v-for="user in file.permissions" :key="user.id">
+
+            <b-col v-for="user in file.permissions" :key="user.id">
                     {{user.displayName}}
+                    {{user.id}}
                 </b-col>
-            <span>---</span>
+            <span>-</span>
             <span><br></span>
             
-            <!--<b-col>{{file.writers}}</b-col>-->
+            <b-col>{{file.writers}}</b-col>
         </b-row>
     </div>
 </template>
@@ -18,13 +20,14 @@
 <script>
 // @ is an alias to /src
 import gapi from "../googleapis.js";
-//gapi.client.drive .files.list
 
 export default {
   name: "folderList",
   async mounted() {
     this.fileList = (await gapi.client.drive.files.list({
-      fields: "files(id, name, permissions)"
+      fields: "files(id, name, permissions)",
+      //q: starred != true
+      q: "'1m0Mq_RHMpXJXfVzEISPySwPheg9PUSqy' in parents" // file id goes here
     })).result.files;
   },
   data() {
