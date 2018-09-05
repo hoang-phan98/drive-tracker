2db7c0e5a6369b3279ae07aee86e0836ca4b7aab
Kenny Ye <kyee0003@student.monash.edu>
2018-09-05 20:18:59 +1000

    Tracked items and charts side-by-side
    Moving elements from the isolated page into the tracked items page

--- src/views/FolderList.vue
+++ src/views/FolderList.vue
@@ -1,33 +1,61 @@
 <template>
-  <b-container fluid class="folderList">
+  <div class="folderList">
+    <b-container fluid class="trackedItems" v-sticky="{ top: 15, zIndex: 0 }">
 
-    <b-row>
-      <h1>Tracked Items</h1>
-    </b-row>
+      <b-row>
+        <h1>Tracked Items</h1>
+      </b-row>
 
-    <b-row>
-      <b-table 
-        :items="folders"
-        :fields="fields"
-        @row-clicked="open($event)"
-        hover
-        >
-      </b-table>
-    </b-row>
+      <b-row>
+        <b-table
+          :items="folders"
+          :fields="fields"
+          @row-clicked="open($event)"
+          hover
+          >
+        </b-table>
+      </b-row>
 
-    <b-row>
-      <b-col class="col2" style="text-align: flex-end">
-        <b-btn v-b-modal.modal1 class="button">+</b-btn>
-      </b-col>
-    </b-row>
+      <b-row>
+        <b-col class="col2" style="text-align: flex-end">
+          <b-btn v-b-modal.modal1 class="button">+</b-btn>
+        </b-col>
+      </b-row>
 
-    <AddFolderModal id="modal1" @add-folder="addFolder($event)" />
-  </b-container>
+      <AddFolderModal id="modal1" @add-folder="addFolder($event)" />
+    </b-container>
+
+    <div class="sideBar">
+      <GChart
+        type="PieChart"
+        :data="pieData"
+        :options="pieOptions"
+      />
+      <GChart
+        type="ColumnChart"
+        :data="histogramData"
+        :options="histogramOptions"
+      />
+      <GChart
+        type="BarChart"
+        :data="barData"
+        :options="barOptions"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import Vue from "vue";
 import AddFolderModal from "./AddFolderModal.vue";
 import googleapis from "../googleapis";
+// This is to make tracked items stick to the top of pagey
+import VueSticky from "vue-sticky";
+Vue.use(VueSticky);
+
+// This is to display the google charts
+import VueGoogleCharts from 'vue-google-charts';
+Vue.use(VueGoogleCharts)
 
 export default {
   name: "FolderList",
@@ -41,7 +69,59 @@ export default {
         { key: "owner", sortable: true, label: "Owner" },
         { key: "lastEdit", sortable: true, label: "Last Edit" }
       ],
-      folders: []
+      folders: [],
+      pieData: [
+        ['Task', 'Hours per Day'],
+        ['Kenny',     11],
+        ['Hoang',      2],
+        ['Erica',  2],
+        ['Dax', 2],
+        ['Marc',    7]
+      ],
+      pieOptions: {
+        title: 'All Time Contribution',
+        pieHole: 0.4,
+        legend: 'none',
+        backgroundColor: { fill:'transparent' }
+      },
+      histogramData: [
+        ['Contributers', 'Add Files', 'Delete Files', 'File Revisions', { role: 'annotation' } ],
+        ['01/01/2018', 10, 24, 20, ''],
+        ['02/01/2018', 16, 22, 23, ''],
+        ['03/01/2018', 28, 19, 29, ''],
+        ['04/01/2018', 16, 22, 23, ''],
+        ['05/01/2018', 28, 19, 29, ''],
+        ['06/01/2018', 10, 24, 20, ''],
+        ['07/01/2018', 16, 22, 23, ''],
+        ['08/01/2018', 28, 19, 29, ''],
+        ['09/01/2018', 16, 22, 23, ''],
+        ['10/01/2018', 10, 24, 20, ''],
+        ['11/01/2018', 16, 22, 23, ''],
+        ['12/01/2018', 28, 19, 29, ''],
+        ['13/01/2018', 16, 22, 23, ''],
+      ],
+      histogramOptions: {
+        title: 'File contrution over time',
+        legend: { position: 'top', maxLines: 3 },
+        bar: { groupWidth: '75%' },
+        isStacked: true,
+        backgroundColor: { fill:'transparent' }
+      },
+      barData: [
+        ['Contributers', 'Add Files', 'Delete Files', 'File Revisions', { role: 'annotation' } ],
+        ['Kenny', 10, 24, 20, ''],
+        ['Hoang', 16, 22, 23, ''],
+        ['Erica', 28, 19, 29, ''],
+        ['Dax', 16, 22, 23, ''],
+        ['Marc', 28, 19, 29, '']
+      ],
+      barOptions: {
+        title: 'User contributions',
+        legend: { position: 'top', maxLines: 3 },
+        bar: { groupWidth: '75%' },
+        isStacked: true,
+        backgroundColor: { fill:'transparent' }
+      },
     };
   },
   methods: {
@@ -74,9 +154,20 @@ export default {
 </script>
 
 <style scoped>
-.folderList {
+.trackedItems {
   background: rgba(255, 255, 255, 0.4);
   padding: 40px;
+  width: 60%;
+  left: 0;
+  position: fixed;
+}
+
+.sideBar {
+  background: rgba(255, 255, 255, 0.4);
+  padding: 40px;
+  width: 40%;
+  right: 0;
+  position: fixed;
 }
 
 .button {
