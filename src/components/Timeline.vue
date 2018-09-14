f79dd432f0284278aeef776359431ba62e1f2680
Kenny <kyee0003@student.monash.edu>
2018-09-14 15:43:19 +1000

    Copy add new google chart component
    created a new google chart component form an existing one using copy and paste, fields need to be editted.

---
+++ src/components/Timeline.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <GChart
+      v-for="file in data"
+      v-bind:key="file.id"
+      type="LineChart"
+      v-bind:data="file.data"
+      :options="file.options"
+    />
+  </div>
+</template>
