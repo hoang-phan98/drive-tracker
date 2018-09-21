1e2dddad38ff5839d28a7a82ca061f35fdb961cc
Erica Son <eson0001@student.monash.edu>
2018-09-21 14:14:51 +1000

    Extracted file data in object 'this.file' in filePage
    Can now access data for charts etc. on the file level

--- src/folder-contributions-service.js
+++ src/folder-contributions-service.js
@@ -2,6 +2,7 @@ import * as a from "awaiting";
 import googleapis from "./googleapis";
 
 const GOOGLE_DRIVE_FOLDER_MIME_TYPE = "application/vnd.google-apps.folder";
+//const GOOGLE_DRIVE_FILE_MIME_TYPE = "application/vnd.google-apps.file";
 const USER_FIELDS = ["kind", "displayName", "emailAddress"].join(", ");
 const REVISION_FIELDS = [
   "kind",
@@ -204,6 +205,35 @@ class FolderContributionsService {
     this.cache.set(folder.id, folder);
 
     return folder;
+  }
+
+  async fetchFileContributionData(id) {
+    // Check the cache first
+    let file;
+    if ((file = this.cache.get(id))) {
+      return file;
+    }
+
+    // Verify the folder actually exists
+    let data1;
+    // try {
+    ({ result: data1 } = await googleapis.client.drive.files.get({
+      fileId: id, ///////find the raw data of the file
+      fields: FileContributions.fields
+    }));
+
+    // if (data1.mimeType !== GOOGLE_DRIVE_FILE_MIME_TYPE) {
+    //   throw new Error("The specified resource is not a file.");
+    //   // }
+    // } catch (err) {
+    //   // TODO :)
+    //   throw err;
+    // }
+
+    file = await FileContributions.create(data1);
+    this.cache.set(file.id, file);
+
+    return file;
   }
 }
 
