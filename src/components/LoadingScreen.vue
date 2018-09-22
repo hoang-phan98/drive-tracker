6c88caa5be985d9cf12457bcfcb56730fef124d4
Kenny Ye <kyee0003@student.monash.edu>
2018-09-22 16:25:34 +1000

    Loading state for folder page
    when folder page is loading, there is a loading animation

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -1,8 +1,51 @@
 <template>
   <div class="LoadingScreen">
-    <img src="../assets/cactus.gif">
-    <progress-wrapper>
-      <progress-bar :value="50" striped animated />
+    <img src="../assets/cactus.gif" class="Cactus">'
+    <progress-wrapper class="Loading" height="30">
+        <progress-bar :value="100" striped animated> <strong>Loading...</strong> </progress-bar>
     </progress-wrapper>
   </div>
 </template>
+
+<script>
+import { Container, Row, Column, ProgressWrapper, ProgressBar, Preloader } from 'mdbvue';
+
+export default {
+  name: 'ProgressBarsProPage',
+  components: {
+    Container,
+    Preloader,
+    Row,
+    Column,
+    ProgressWrapper,
+    ProgressBar,
+    Preloader
+  }
+};
+</script>
+
+<style>
+.LoadingScreen {
+  height: 100%;
+  width: 100%;
+  background-color: white;
+}
+
+.Cactus {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 2%;
+  width: 35%;
+}
+
+.Loading {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 25%;
+  height: 1000;
+  position: element(#Target);
+  transform: translateY(-700%);
+}
+</style>
