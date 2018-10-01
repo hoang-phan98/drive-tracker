941182375c725d4a95d07fefb3629f6a6bf80efe
Hoang Phan <hpha0005@student.monash.edu>
2018-10-01 19:57:54 +1000

    Linking File Contribution data to Pie Chart
    created a new component for file level pie chart as folder level pie chart expects an array of files

---
+++ src/components/PieChartFile.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <GChart
+      type="PieChart"
+      :data="data"
+      :options="pieOptions"
+    />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueGoogleCharts from "vue-google-charts";
+
+Vue.use(VueGoogleCharts);
+
+export default {
+  name: "PieChartFile",
+  props: {
+    revisions: Array,
+    contributions: Array,
+    colors: Object,
+    contributors: Array,
+    height: Number
+  },
+  computed: {
+    data() {
+      var result = [["User", "Contributions"]];
+      for (let user of this.contributors) {
+        const name = user.displayName;
+        const values = this.contributions.filter(
+          contribution => contribution.user.emailAddress === user.emailAddress
+        ).length;
+
+        var total = 0;
+        for (var i = 0; i < values.length; i++) {
+          total = total + values[i];
+        }
+        result.push([name, total]);
+      }
+      return result;
+    },
+    pieOptions() {
+      return {
+        chartArea: {
+          width: "85%"
+        },
+        height: this.height || 500,
+        title: "All Time Contribution",
+        pieHole: 0.4,
+        legend: "none",
+        colors: this.contributors.map(user => this.colors[user.emailAddress])
+      };
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/FilePage.vue
+++ src/views/FilePage.vue
@@ -24,10 +24,12 @@
         <!-- </div> -->
       </div>
       <div class = "pichart">
-        <GChart
-          type="PieChart"
-          :data="pieData"
-          :options="pieOptions"
+        <PieChartFile
+          v-if="file"
+          :revisions ="Object.values(file.revisions)"
+          :contributions="Object.values(file.contributions)"
+          :contributors="Object.values(file.contributors)"
+          :colors="colors"
         />
 
       </div>
@@ -93,6 +95,7 @@ import VueGoogleCharts from "vue-google-charts";
 import Colours from "./ColourGeneration.vue";
 import MaterialIcon from "@/components/MaterialIcon.vue";
 import LoadingScreen from "../components/LoadingScreen.vue";
+import PieChartFile from "../components/PieChartFile.vue";
 //import randomColour from "./ColourGeneration.vue";
 
 Vue.use(VueGoogleCharts);
@@ -102,7 +105,8 @@ export default {
   name: "FilePage",
   components: {
     MaterialIcon,
-    LoadingScreen
+    LoadingScreen,
+    PieChartFile
   },
   props: {
     id: String,
@@ -145,7 +149,6 @@ export default {
     });
 
     // call generate colours function while passing in the number of users from userList.length
-    this.populatePieData();
     this.populateBarData();
 
     this.file = file;
@@ -185,22 +188,6 @@ export default {
       }
 
       this.barOptions.colors = this.colourList;
-    },
-    populatePieData() {
-      for (let i = 0; i < this.userList.length; i++) {
-        let data = [];
-        data.push(this.userList[i]);
-        data.push(this.pieStats[i]);
-        data.push(this.colourList[i]);
-
-        //console.log([(this.userList[i], this.pieStats[i], this.colourList[i])]);
-        this.pieData.push([
-          this.userList[i],
-          this.pieStats[i]
-          //this.colourList[i]
-        ]);
-        this.pieOptions.colors = this.colourList;
-      }
     }
   },
   data() {
@@ -251,7 +238,6 @@ export default {
         ["Dax", 16, "#FFFF00	"],
         ["Marc", 28, "#808080"]
       ],*/
-      pieStats: [4, 2, 5, 7, 10, 2, 3, 4],
       barStats: [],
       userOptions: {
         //width: 600,
@@ -260,25 +246,6 @@ export default {
         legend: "none",
         bar: { groupWidth: "75%" },
         isStacked: "percent"
-      },
-      pieData: [
-        ["Task", "Hours per Day"]
-        // ["Kenny", 11],
-        // ["Hoang", 2],
-        // ["Erica", 2],
-        // ["Dax", 2],
-        // ["Marc", 7]
-      ],
-      pieOptions: {
-        //width: 600,
-        chartArea: {
-          width: "85%"
-        },
-        height: this.height || 500,
-        title: "All Time Contribution",
-        pieHole: 0.4,
-        legend: "none"
-        //colors: colourList
       },
       histogramData: [
         [
