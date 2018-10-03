36b0003e1d714fe5a48bac40333e353c4679787c
Fenghe Shen <fshe17@student.monash.edu>
2018-10-04 01:16:57 +1000

    Fixed issue where Timeline dates are not sorted
    Reformatted Timeline date legends to make them shorter.
    Added dateformat package.

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -16,6 +16,7 @@
 <script>
 import Vue from "vue";
 import VueGoogleCharts from "vue-google-charts";
+import dateFormat from "dateformat";
 
 Vue.use(VueGoogleCharts);
 
@@ -32,7 +33,7 @@ export default {
       const lineData = [];
 
       // loop the the files of the folder in order to get the number of lines\
-      const labels = ["Contributor"];
+      const labels = ["Time"];
       for (var z = 0; z < this.contributors.length; z++) {
         labels.push(this.contributors[z].displayName);
       }
@@ -46,11 +47,13 @@ export default {
         values.push(0);
       }
 
-      // increments which file contained that revision and then pushes the row
+      /*// increments which file contained that revision and then pushes the row
       for (var i = 0; i < this.files.length; i++) {
         for (var j = 0; j < this.files[i].revisions.length; j++) {
-          values[0] = this.files[i].revisions[j].modifiedTime;
-
+          var dateString = this.files[i].revisions[j].modifiedTime;
+          //values[0] = dateFormat(dateString, "dd/mm");
+          values[0] = dateString;
+          console.log(typeof(dateString));
           // Now to increment the revision for the corresponder contributor
           for (var h = 0; h < this.contributors.length; h++) {
             if (
@@ -61,12 +64,49 @@ export default {
             }
           }
           var duplicateObject = JSON.parse(JSON.stringify(values));
-          lineData.push(duplicateObject);
+          lineData.push(values);
+        }
+      }*/
+
+      // Get all revisions first
+      var totalRevisions = [];
+      for (var f = 0; f < this.files.length; f++) {
+        var file = this.files[f];
+
+        for (var r = 0; r < file.revisions.length; r++) {
+          var revision = file.revisions[r];
+          totalRevisions.push(revision);
         }
       }
 
+      // Sort all revisions by date
+      totalRevisions.sort(function(a, b) {
+        return new Date(a.modifiedTime) - new Date(b.modifiedTime);
+      });
+
+      // Increment revision count to corresponding user
+      for (var tr = 0; tr < totalRevisions.length; tr++) {
+        for (var c = 0; c < this.contributors.length; c++) {
+          if (
+            this.contributors[c].displayName ===
+            totalRevisions[tr].lastModifyingUser.displayName
+          ) {
+            var dateString = dateFormat(
+              totalRevisions[tr].modifiedTime,
+              "dd/mm"
+            );
+            values[0] = dateString;
+            values[c + 1] += 1;
+          }
+        }
+
+        // Add entry of that date to chart
+        var parse = JSON.parse(JSON.stringify(values));
+        lineData.push(parse);
+      }
+
       // If you want to see what the line data looks like uncomment below.
-      // console.log(lineData);
+      //console.log(lineData);
 
       return {
         data: lineData,
