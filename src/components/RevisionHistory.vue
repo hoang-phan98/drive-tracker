70354976e960d9d3f6af93e017eeecd07c4091d5
Kenny Ye <kyee0003@student.monash.edu>
2018-10-04 14:21:49 +1000

    Date and time formatting
    Fixed the date and time formatting so it looks good and nice

--- src/components/RevisionHistory.vue
+++ src/components/RevisionHistory.vue
@@ -21,15 +21,37 @@ export default {
       const items = [];
 
       for (var i = 0; i < this.revisions.length; i++) {
+        //Retriving the date and formating it correctly
+        var noon = "am";
+        var date = new Date(this.revisions[i].modifiedTime).toDateString();
+        var hour = new Date(this.revisions[i].modifiedTime).getHours();
+        if (hour > 12) {
+          hour -= 12;
+          noon = "pm";
+        };
+        if (hour == 0) {
+          hour = 12;
+        };
+        if (hour.toString().length == 1) {
+          hour = "0" + hour;
+        };
+        var min = new Date(this.revisions[i].modifiedTime).getMinutes();
+        if (min.toString().length == 1) {
+          min = "0" + min;
+        };
+
+        console.log(typeof hour);
+
         var builder = {
           "Revision ID": this.revisions[i].id,
-          "Modified Time": this.revisions[i].modifiedTime,
+          "Modified Date": date,
+          "Modified Time": hour + ":" + min + " " + noon,
           "Last Modifying User": this.revisions[i].lastModifyingUser.displayName
-        };
+        }
         items.push(builder);
       }
       return {
-        fields: ["Revision ID", "Modified Time", "Last Modifying User"],
+        fields: ["Revision ID", "Modified Date", "Modified Time", "Last Modifying User"],
         items: items
       };
     }
